405132f82e452b3559015fa5338225caa6f81935
Mikaël Capelle <capelle.mikael@gmail.com>
2020-05-26 21:43:01 +0200

    Custom ModDataContent.

---
+++ src/falloutnvmoddatacontent.h
@@ -0,0 +1,21 @@
+#ifndef FALLOUTNV_MODDATACONTENT_H
+#define FALLOUTNV_MODDATACONTENT_H
+
+#include <gamebryomoddatacontent.h>
+#include <ifiletree.h>
+
+class FalloutNVModDataContent : public GamebryoModDataContent {
+public:
+
+  /**
+   *
+   */
+  FalloutNVModDataContent(GameGamebryo const* gamePlugin) : GamebryoModDataContent(gamePlugin) {
+    // Just need to disable some contents:
+    m_Enabled[CONTENT_MCM] = false;
+    m_Enabled[CONTENT_SKYPROC] = false;
+  }
+
+};
+
+#endif // FALLOUTNV_MODDATACONTENT_H
