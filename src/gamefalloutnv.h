c47b5151546b81f19344fc25fe3ae8d2171ad1de
Jeremy Rimpo <jrim@rimpo.org>
2023-09-28 11:25:05 -0500

    Preliminary EPIC Games support - Requires gamebryo game detection pull request

--- src/gamefalloutnv.h
+++ src/gamefalloutnv.h
@@ -8,48 +8,60 @@
 
 class GameFalloutNV : public GameGamebryo
 {
-  Q_OBJECT
+	Q_OBJECT
 #if QT_VERSION >= QT_VERSION_CHECK(5,0,0)
-  Q_PLUGIN_METADATA(IID "org.tannin.GameFalloutNV" FILE "gamefalloutnv.json")
+		Q_PLUGIN_METADATA(IID "org.tannin.GameFalloutNV" FILE "gamefalloutnv.json")
 #endif
 
 public:
+	GameFalloutNV();
 
-  GameFalloutNV();
-
-  virtual bool init(MOBase::IOrganizer *moInfo) override;
+	virtual bool init(MOBase::IOrganizer* moInfo) override;
 
 public: // IPluginGame interface
+	virtual QString gameName() const override;
+	virtual void detectGame() override;
+	virtual QList<MOBase::ExecutableInfo> executables() const override;
+	virtual QList<MOBase::ExecutableForcedLoadSetting> executableForcedLoads() const override;
+	virtual void initializeProfile(const QDir& path, ProfileSettings settings) const override;
+	virtual QString steamAPPId() const override;
+	virtual QStringList primaryPlugins() const override;
+	virtual QStringList gameVariants() const override;
+	virtual QString gameShortName() const override;
+	virtual QStringList validShortNames() const override;
+	virtual QString gameNexusName() const override;
+	virtual QStringList iniFiles() const override;
+	virtual QStringList DLCPlugins() const override;
+	virtual int nexusModOrganizerID() const override;
+	virtual int nexusGameID() const override;
 
-  virtual QString gameName() const override;
-  virtual void detectGame() override;
-  virtual QList<MOBase::ExecutableInfo> executables() const override;
-  virtual QList<MOBase::ExecutableForcedLoadSetting> executableForcedLoads() const override;
-  virtual void initializeProfile(const QDir &path, ProfileSettings settings) const override;
-  virtual QString steamAPPId() const override;
-  virtual QStringList primaryPlugins() const override;
-  virtual QString gameShortName() const override;
-  virtual QStringList validShortNames() const override;
-  virtual QString gameNexusName() const override;
-  virtual QStringList iniFiles() const override;
-  virtual QStringList DLCPlugins() const override;
-  virtual int nexusModOrganizerID() const override;
-  virtual int nexusGameID() const override;
+	virtual bool isInstalled() const override;
+	virtual void setGamePath(const QString& path) override;
+	virtual QDir gameDirectory() const override;
 
 public: // IPlugin interface
-
-  virtual QString name() const override;
-  virtual QString localizedName() const override;
-  virtual QString author() const override;
-  virtual QString description() const override;
-  virtual MOBase::VersionInfo version() const override;
-  virtual QList<MOBase::PluginSetting> settings() const override;
+	virtual QString name() const override;
+	virtual QString localizedName() const override;
+	virtual QString author() const override;
+	virtual QString description() const override;
+	virtual MOBase::VersionInfo version() const override;
+	virtual QList<MOBase::PluginSetting> settings() const override;
+	virtual MappingType mappings() const override;
 
 protected:
+	QString gameDirectoryName() const;
+	QDir documentsDirectory() const;
+	QDir savesDirectory() const;
+	QString myGamesPath() const;
 
-  virtual QString savegameExtension() const override;
-  virtual QString savegameSEExtension() const override;
-  std::shared_ptr<const GamebryoSaveGame> makeSaveGame(QString filePath) const override;
+	void setVariant(QString variant);
+	void checkVariants();
+
+protected:
+	virtual QString identifyGamePath() const override;
+	virtual QString savegameExtension() const override;
+	virtual QString savegameSEExtension() const override;
+	std::shared_ptr<const GamebryoSaveGame> makeSaveGame(QString filePath) const override;
 
 };
 
